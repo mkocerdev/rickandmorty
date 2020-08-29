a827bc67cefe4093a7e16e91e5352d420d7316f3
mehmetkocer <mehmetkocer2@hotmail.com>
2020-08-29 19:04:59 +0300

    Font and global css added

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      {{ characters }}
-    </div>
+  <div>
+    <nuxt-link to="/characters">Characters</nuxt-link>
+    <nuxt-link to="/episodes">Episodes</nuxt-link>
+    <nuxt-link to="/locations">Locations</nuxt-link>
   </div>
 </template>
 
 <script>
-import charactersQuery from '~/apollo/queries/characters'
-
-export default {
-  data() {
-    return {
-      characters: [],
-    }
-  },
-  apollo: {
-    characters: {
-      prefetch: true,
-      query: charactersQuery,
-    },
-  },
-}
+export default {}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
