dd7d1d0c3374d8e79960e604254608eeabe7f136
mehmetkocer <mehmetkocer2@hotmail.com>
2020-09-04 00:19:20 +0300

    Header dark, light and github icons added

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-white dark:bg-black">
+  <div class="bg-white dark:bg-gray-900">
     <div class="container mx-auto flex items-center">
       <div class="logo w-2/12 float-left">
         <nuxt-link to="/"><Logo class="w-full" /></nuxt-link>
       </div>
       <div class="menu w-10/12 float-left">
-        <ul class="float-right">
+        <ul class="float-right flex items-center">
           <li class="float-left px-4 text-black dark:text-white">
             <nuxt-link to="/characters">Characters</nuxt-link>
           </li>
@@ -17,17 +17,31 @@
           </li>
           <li class="float-left px-4 text-black dark:text-white">
             <a
-              href="https://github.com/mehmetkocerr/rickandmorty"
+              href="https://github.com/mkocerdev/rickandmorty"
               target="__blank"
-              >Github</a
             >
+              <githubIcon class="w-8 text-black dark:text-white" />
+            </a>
           </li>
           <li class="float-left px-4 text-black dark:text-white">
-            <button
-              class="btn border capitalize border-black dark:border-white"
-              @click="changeMode"
-            >
-              {{ btnLabel }}
+            <button class="flex items-center" @click="changeMode">
+              <p
+                class="flex items-center px-5 py-3 bg-gray-300 dark:bg-black rounded-full transition duration-300 ease"
+              >
+                <img
+                  v-if="$colorMode.preference === 'light'"
+                  class="w-6"
+                  src="~/assets/images/dark.png"
+                  alt="Dark Mode"
+                />
+                <img
+                  v-else
+                  class="w-6"
+                  src="~/assets/images/light.png"
+                  alt="Light Mode"
+                />
+                <span class="pl-2"> {{ mode }} Mode </span>
+              </p>
             </button>
           </li>
         </ul>
@@ -37,13 +51,17 @@
 </template>
 
 <script>
+import githubIcon from '~/components/icon/github'
 export default {
+  components: {
+    githubIcon,
+  },
   data() {
     return {}
   },
   computed: {
-    btnLabel() {
-      return this.$colorMode.preference === 'light' ? 'dark' : 'light'
+    mode() {
+      return this.$colorMode.preference === 'light' ? 'Dark' : 'Light'
     },
   },
   methods: {
