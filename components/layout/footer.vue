75f1b521c4dacf5350bc980aef15c8f189e2eb45
mehmetkocer <mehmetkocer2@hotmail.com>
2020-09-04 00:17:07 +0300

    Footer component added and usage default layout

---
+++ components/layout/footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white dark:bg-gray-900 mt-8">
+    <div class="container mx-auto flex items-center py-12">
+      <p class="w-full block text-right text-gray-500 dark:text-gray-600">
+        Made by
+        <a
+          href="https://github.com/mkocerdev"
+          target="__blank"
+          class="text-gray-800 dark:text-gray-300"
+          >@mkocerdev</a
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,16 @@
   <div class="w-full">
     <Header />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 <script>
 import Header from '~/components/layout/header'
+import Footer from '~/components/layout/footer'
 export default {
   components: {
     Header,
+    Footer,
   },
 }
 </script>
